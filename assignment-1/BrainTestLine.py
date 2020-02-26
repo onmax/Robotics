4f95418f48bb46d3df904e476601c650e8cf04b6
onmax <maximogarciamtnez@gmail.com>
2020-02-26 20:33:51 +0100

    making some progress following the line

--- assignment-1/BrainTestLine.py
+++ assignment-1/BrainTestLine.py
@@ -1,41 +1,80 @@
 from pyrobot.brain import Brain
 
 import math
-
+import numpy as np
 
 class BrainTestNavigator(Brain):
-    NO_FORWARD = 0
-    SLOW_FORWARD = 0.1
-    MED_FORWARD = 0.5
-    FULL_FORWARD = 1.0
+    NO_FORWARD = 0.05
+    SLOW_FORWARD = 0.35
+    MED_FORWARD = 0.6
+    FULL_FORWARD = 1
 
     NO_TURN = 0
-    MED_LEFT = 0.5
-    HARD_LEFT = 1.0
-    MED_RIGHT = -0.5
-    HARD_RIGHT = -1.0
 
-    NO_ERROR = 0
+    NO_LEFT = 0
+    SLOW_LEFT = 0.1
+    MED_LEFT = 0.45
+    HARD_LEFT = 0.8
+
+    NO_RIGHT = 0
+    SLOW_RIGHT = -0.1
+    MED_RIGHT = -0.45
+    HARD_RIGHT = -0.8
+
+    STATES = [
+        { "min": 1, "max": 0.6, "forward": NO_FORWARD, "direction": HARD_LEFT},
+        { "min": 0.6, "max": 0.3, "forward": SLOW_FORWARD, "direction": MED_LEFT},
+        { "min": 0.3, "max": 0.2, "forward": MED_FORWARD, "direction": SLOW_LEFT},
+        { "min": 0.2, "max": -0.2, "forward": FULL_FORWARD, "direction": NO_TURN},
+        { "min": -0.2, "max": -0.3, "forward": MED_FORWARD, "direction": SLOW_RIGHT},
+        { "min": -0.3, "max": -0.6, "forward": SLOW_FORWARD, "direction": MED_RIGHT},
+        { "min": -0.6, "max": -1, "forward": NO_FORWARD, "direction": HARD_RIGHT},
+    ]
+
+    # As in the slides
+    PREVIOUS_ERROR = 0
+    INTEGRAL = 0
+
+    # This variable stores the state that was used in the previous tick 
+    LAST_STATE = STATES[3]
+
+    # This is how many ticks the robot has been lost
+    # Specifically, the number of ticks that have been missing without finding the line
+    N_TICKS_LOST = 0
 
     def setup(self):
         pass
 
     def step(self):
-        hasLine, lineDistance, searchRange = eval(
+        line_is_visible, error, searchRange = eval(
             self.robot.simulation[0].eval("self.getLineProperties()"))
-        print("I got from the simulation", hasLine, lineDistance, searchRange)
+        print("Line: {}. Distance: {}. Search range: {}".format(line_is_visible, error, searchRange))
 
-        if (hasLine):
-            if (lineDistance > self.NO_ERROR):
-                self.move(self.FULL_FORWARD, self.HARD_LEFT)
-            elif (lineDistance < self.NO_ERROR):
-                self.move(self.FULL_FORWARD, self.HARD_RIGHT)
-            else:
-                self.move(self.FULL_FORWARD, self.NO_TURN)
+        # We might use this variables
+        derivative = error - self.PREVIOUS_ERROR
+        self.INTEGRAL += error
+
+        if line_is_visible:
+            self.N_TICKS_LOST = 0
+
+            # This part calculates the amount of throttle and the amount of steering that 
+            # needs to be applied in order to minimaze the error (go to the center of the
+            # line)
+            normalized_error = error / searchRange
+            for state in self.STATES:
+                if state["min"] >= normalized_error > state["max"]:
+                    self.move(state["forward"], state["direction"])
+                    self.LAST_STATE = state
         else:
-            # if we can't find the line we just stop, this isn't very smart
-            self.move(self.NO_FORWARD, self.NO_TURN)
+            # If the line is not found, the robot won't move, it is going to steer
+            # to the direction where the line was seen last time
+            if self.N_TICKS_LOST < 14:
+                steering = 0.4
+                direction = steering if self.LAST_STATE["direction"] < 0 else -steering
+                self.move(0,direction)
+                self.N_TICKS_LOST += 1
 
+        self.PREVIOUS_ERROR = error
 
 def INIT(engine):
     assert (engine.robot.requires("range-sensor") and
--- assignment-1/lineTestWorld.py
+++ assignment-1/lineTestWorld.py
@@ -6,23 +6,26 @@ from pyrobot.simulators.lineSimulation import LineSimulation
 from pyrobot.simulators.pysim import TkPioneer, \
      PioneerFrontSonars, PioneerFrontLightSensors
 
+
+
 def INIT():
+    background = 3
+    positions = [[1, 18.9, 4.0], [8.5, 2.35, 1.57], [10.5, 19, 8.2]]
+
     # (width, height), (offset x, offset y), scale
-    sim = LineSimulation((450,675), (20,650), 32,
-                             background="./lineBackground1.png")  
-                             # background="./lineBackground2.png")  
+    sim = LineSimulation((450, 675), (20, 650), 32,
+                         background="./lineBackground{}.png".format(background))
 
     # an example of an obstacle on the line
-      # x1, y1, x2, y2
+    # x1, y1, x2, y2
     # sim.addBox(5, 12, 6, 11)
-
-    sim.addRobot(60000, 
-		 # name, x, y, th, boundingBox
-                 TkPioneer("RedErratic", 
-			   # position for lineBackground-1
-		           1, 18.9, 4.0,
-			   # position for lineBackground-2
-		           # 8.5, 2.35, 1.57,
+    print("position", positions[background-1])
+    sim.addRobot(60000,
+                 # name, x, y, th, boundingBox
+                 TkPioneer("RedErratic",
+                           positions[background-1][0], 
+                           positions[background-1][1],
+                           positions[background-1][2],
                            ((.185, .185, -.185, -.185),
                             (.2, -.2, -.2, .2))))
 
@@ -30,6 +33,6 @@ def INIT():
     sim.robots[0].addDevice(PioneerFrontSonars())
 
     # to create a trail
-    # sim.robots[0].display["trail"] = 1
+    sim.robots[0].display["trail"] = 1
 
     return sim
